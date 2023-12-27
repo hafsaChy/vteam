703e076937d6bdfb19d73ee8e75691bc8cd0c9d7
hafsaChy <hafsa05mat@gmail.com>
2023-12-27 12:44:28 +0100

    updated test

--- server/test/server/cityRoutes.test.js
+++ server/test/server/cityRoutes.test.js
@@ -24,7 +24,7 @@ describe('City Routes', function () {
             host: process.env.MYSQL_HOST,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
-            // database: process.env.MYSQL_DATABASE,
+            database: process.env.MYSQL_DATABASE,
             database: test_elcyckel,
             connectionLimit: 20,
             connectTimeout: 30000,
